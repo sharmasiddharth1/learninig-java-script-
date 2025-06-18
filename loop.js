9081d859b80e731f6ce4df10a946aaee4363d729
siddharth sharma <sharmasiddharth9717@gmail.com>
2025-06-18 18:53:06 +0530

    Add loop.js with while and for loop practice

---
+++ loop.js
@@ -0,0 +1,22 @@
+var myname = "Siddharth sharma";
+myname = "Sugam sharma";
+console.log(myname)
+
+
+
+let n = 1 ;
+let sum = 0 ;
+while(n <=10){
+    sum  = sum + n;
+    n++;
+}
+console.log(sum);
+
+//practise question 
+// 1) :   for loop 
+let answer = 0 ;
+
+for(let i = 0 ; i<=1000 ; i++){
+    answer += i ;
+}
+console.log(answer);
