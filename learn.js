3244bbf013b4d7406bc500dae73c175a56107ca8
siddharth sharma <sharmasiddharth9717@gmail.com>
2025-06-18 18:54:26 +0530

    this is an empty commit

---
+++ learn.js
@@ -0,0 +1,16 @@
+// console.log("hello world this is my first program of java script : ");
+let firstname = "siddharth sharma"
+let age = 22 ; 
+let ismarried = false ;
+
+
+// console.log(" here the owner of this program file who's name is : " + firstname  )
+
+
+if(ismarried){
+    console.log(" this person is  married : hahaha ");
+}
+else{
+    console.log("this person, i'mean me not married soo bad ");
+}
+
