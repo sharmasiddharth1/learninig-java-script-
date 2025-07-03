ee21c40ee99bcc0e3470f8ab7fe37efb1fdba8fc
siddharth sharma <sharmasiddharth9717@gmail.com>
2025-07-03 16:49:15 +0530

    learn some arrays methods

---
+++ DAY3/arraymethod.js
@@ -0,0 +1,38 @@
+let cars = ["tata", "bmw" , "marcesdeies" , "maruti"];
+// push
+// add methods use to add element or data in the last of the array
+console.log(cars)
+cars.push("xuv")
+console.log(cars);
+
+// we can add as many we want all are add to the last of the arrays 
+cars.push("toyota" , "farrari");
+console.log(cars);
+
+
+// pop for the removing the element form the last 
+//'here we can pass the argument inside the pop method'
+cars.pop();
+console.log(cars);
+cars.pop();
+console.log(cars);
+
+
+// unshift for the adding element in front of the arrays
+
+cars.unshift("raajdut");
+console.log(cars);
+cars.unshift("mgvector");
+console.log(cars);
+
+// shift for the remove the element from the top most arrays 
+
+cars.shift();
+cars.shift();
+
+
+console.log(cars);
+
+
+
+
