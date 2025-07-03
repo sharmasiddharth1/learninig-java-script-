6e3a732b3317f42bbcb7ca764817994d164a35c9
siddharth sharma <sharmasiddharth9717@gmail.com>
2025-07-03 21:06:38 +0530

    update : updating the shorting array file.

--- DAY3/short_array.js
+++ DAY3/short_array.js
@@ -6,4 +6,8 @@ let Name = ["arti", "preeti" , "adarsh" , "siddharth", "satyam", "arman"];
 
 
 console.log(Name.sort());
+// this will not work same as for the numbers 
+let Nums = [1 , 99 , 43, 55, 2, 67, 42, 9 , 10 ]
+Nums.sort();
+console.log(Nums);
 
