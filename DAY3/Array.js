9be906281721b8f4bd120ea1634faf1232487772
siddharth sharma <sharmasiddharth9717@gmail.com>
2025-07-03 16:14:19 +0530

    init array file

---
+++ DAY3/Array.js
@@ -0,0 +1,14 @@
+let info = ["siddharth" , 22 , "Ai" , "4th" , 2201331529015];
+console.log(info);
+let n = info.length;
+console.log(n)
+console.log(info[3].length);
+console.log(typeof info[0]);
+console.log(typeof info[1]);
+
+console.log(typeof info[2]);
+
+
+
+
+ 
