9356e5fdf8078e5fd4000d34a6e0eb2897955360
siddharth sharma <sharmasiddharth9717@gmail.com>
2025-07-16 14:22:17 +0530

    arrays is call by refference

---
+++ DAY3/arrays_reffernce_adds.js
@@ -0,0 +1,10 @@
+let arr = [2 , 3, 4, 5,56, 7, 7,  , 45 ];
+let arr2 = arr;
+
+
+// .. array 1 store at the address of the array which is created 
+// ....... name of the array variable only holds the value of refference and address of the actual address of the array were stored 
+
+console.log(arr2);
+console.log(arr);
+console.log(arr2.length);
