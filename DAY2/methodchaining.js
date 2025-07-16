97c3299dbf792d4f7bdf8a7da32aa6d76200bd17
siddharth sharma <sharmasiddharth9717@gmail.com>
2025-07-16 13:45:45 +0530

    use multiple methods in single chain

---
+++ DAY2/methodchaining.js
@@ -0,0 +1,10 @@
+// chaining means kee do se ek ya ek se jyda yani do methods ek sath use karna 
+
+
+let msg = " i beg to say that i am student of the btech department . ";
+let name1 = "siddharth sharma      ";
+let intro = "i am "
+
+console.log(name1 + msg );
+console.log(intro + name1.toUpperCase().trim() + msg);
+
