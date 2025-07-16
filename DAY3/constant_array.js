ce73eac7da5f6ca0eba893129ae3a941464c0db4
siddharth sharma <sharmasiddharth9717@gmail.com>
2025-07-16 14:22:37 +0530

    constant array defination

---
+++ DAY3/constant_array.js
@@ -0,0 +1,21 @@
+// constant arrays  
+// constant arrays means making an array a constant which cant able to change
+
+
+// changes mean for only the whole array not their element ;
+// stil do the operation on their elements 
+
+const name = ["siddharth" , "shreya" ,"anshuman" , 4 ];
+console.log(name);
+name[3] = "somnath";
+console.log(name);
+
+name.push("karishma");
+console.log(name);
+
+name.push("vishal singh");
+console.log(name);
+
+console.log(name.pop());
+
+
