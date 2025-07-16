996476ffdba05071643ac423168babda2d55b127
siddharth sharma <sharmasiddharth9717@gmail.com>
2025-07-16 13:46:21 +0530

    to get particular item from the srting use "slice"

---
+++ DAY2/slice_method.js
@@ -0,0 +1,13 @@
+let str = "i love someone which have same interest";
+str.slice(2);
+
+console.log(str.slice( 7));
+console.log(str.slice(2,6));
+
+let college = "noida institute of technology";
+console.log(college.slice(6 , 16))
+
+
+let name = "siddharth";
+console.log(name.slice(-1)) // 9 - 1 =8 
+console.log(name.slice(8));
