21f3177cc1aad2f60aa1cd7e21be7645cae6bbfa
siddharth sharma <sharmasiddharth9717@gmail.com>
2025-07-16 13:47:08 +0530

    replace and repeat methods for the string

---
+++ DAY2/replace_repeat.js
@@ -0,0 +1,10 @@
+ let greet = "tony stark";
+ // replace 
+// for the replace the word charcter or any thing from the string 
+ console.log(greet);
+ console.log( greet.replace("stark" , "man"));
+
+ // repeat 
+ // for the repeating the string 
+
+ console.log(greet.repeat(4));
