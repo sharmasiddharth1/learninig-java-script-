d79f508ce2664db7a06239d92820eaa0208f8aed
siddharth sharma <sharmasiddharth9717@gmail.com>
2025-07-16 20:38:21 +0530

    for loop syntax

---
+++ DAY4/for_loop.js
@@ -0,0 +1,16 @@
+// for loop
+// way to write for loop 
+
+/**
+ 
+for ( intialize ; comparrison or condtion ; updation or update ){
+    code revolve around their condition 
+}
+
+**/
+
+
+for(let x = 1 ; x <= 10 ; x++){
+    console.log(x);
+}
+let abc = prompt(input)
